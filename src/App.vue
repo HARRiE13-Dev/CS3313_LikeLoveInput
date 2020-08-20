ce675a2dfc38854bc276ab93bc95d33944bded6c
HARRiE13-Dev <62064385+HARRiE13-Dev@users.noreply.github.com>
2020-08-20 19:31:33 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+   <img src="./assets/logo.png" alt="">
+    <HelloWorld msg="VUE Project Counter & INPUT"/>
+    <h3>Surachart Khongkamnert</h3>
+    <h4>__________________________________________________</h4><br>
+    <p>{{counter}}</p>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <counter label='like'/>
+    </div>
+  </div>
+</div>
+
+<h4>__________________________________________________</h4><br>
+
+<h4>Name <input type="text" v-model ="name"></h4> 
+<h4>Code <input type="text" v-model ="code"></h4>
+<h4>GPA <input type="number" v-model ="gpa"></h4>
+
+<br>
+<h3> - Output -</h3> <br>
+
+<p>{{name}} {{code}} {{gpa}}</p>
+
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+import counter from './components/counter.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    counter
+  },
+
+
+  data:function (){
+    return{
+      name: "",
+      code: "",
+      gpa: 0.0
+
+    };
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/counter.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="example-1">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+           <button v-on:click="$like" class="btn btn-primary btn-lg" >
+      <i class="far fa-thumbs-up"></i>LIKE
+    </button>
+        </div>
+        <div class="col-sm">
+          <button v-on:click="$love" class="btn btn-danger btn-lg" >
+      <i class="far fa-thumbs-up"></i>LOVE
+    </button>
+        </div>
+        <div class="col-sm">
+           <button v-on:click="$lol" class="btn btn-warning btn-lg" >
+      <i class="far fa-thumbs-up"></i>LOL
+    </button>
+        </div>
+      </div>
+    </div>
+  
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <h4>LIKE : {{ like }}</h4>
+        </div>
+        <div class="col-sm">
+          <h4>LOVE : {{ love }}</h4>
+        </div>
+        <div class="col-sm">
+          <h4>LOL : {{ lol }}</h4><br>
+        </div>
+  </div>
+</div>
+<h2>Total Score : {{ counter }}</h2>
+<br>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+      like: 0,
+      love: 0,
+      lol: 0,
+      counter: 0,
+      
+    };
+  },
+
+  methods: {
+    $like: function() {
+      this.counter += 1;
+      this.like += 1;
+      this.$emit("cc", this.counter);
+    },
+    $love: function() {
+      this.counter += 5;
+      this.love += 1;
+      this.$emit("cc", this.counter);
+    },
+    $lol: function() {
+      this.counter += 10;
+      this.lol += 1;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+
+<style></style>
